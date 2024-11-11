ba99771d4f815f71d1089d045c91a2771dd355b0
Luke Mathews-Patience <lukemathewsp1@gmail.com>
2024-11-11 22:36:25 +0000

    test

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero({ pageInfo }: Props) {
   const [text] = useTypewriter({
     words: [
       `Hi, I'm ${pageInfo?.name}`,
-      "I'm a Software Engineer",
+      "I'm a Product Owner",
       "And I run marathons />",
     ],
     loop: true,
